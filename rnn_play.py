263e633c222eae51de71bd591244a34fb76de487
chiriacandrei25 <chiriacandrei25@yahoo.com>
2018-07-17 14:05:14 +0300

    Add files via upload

---
+++ rnn_play.py
@@ -0,0 +1,80 @@
+# encoding: UTF-8
+# Copyright 2017 Google.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import tensorflow as tf
+import numpy as np
+import my_txtutils
+import checker
+
+# these must match what was saved !
+ALPHASIZE = my_txtutils.ALPHASIZE
+directory = "C:/Users/achiriac/Desktop/validation_test/VALIDATION.txt"
+NLAYERS = 3
+INTERNALSIZE = 512
+
+eC0 = "checkpoints/rnn_train_1531300337-10000000"
+eC1 = "checkpoints/rnn_train_1531305941-20000000"
+eC2 = "checkpoints/rnn_train_1531816581-10000000"
+
+author = eC2
+
+
+def validate_test():
+    valitext, _, __ = my_txtutils.read_data_files(directory, validation=False)
+
+    if (checker.check_bugs(targetfile=directory)):
+        print("This is a bug!")
+    else:
+        VALI_SEQLEN = 1 * 64  # Sequence length for validation. State will be wrong at the start of each sequence.
+        bsize = len(valitext) // VALI_SEQLEN
+        vali_x, vali_y, _ = next(
+            my_txtutils.rnn_minibatch_sequencer(valitext, bsize, VALI_SEQLEN, 1))  # all data in 1 batch
+        vali_nullstate = np.zeros([bsize, INTERNALSIZE * NLAYERS])
+        feed_dict = {'inputs/X:0': vali_x, 'target/Y_:0': vali_y, 'model/pkeep:0': 1.0,
+                     'hidden_state/Hin:0': vali_nullstate, 'model/batchsize:0': bsize}
+
+        ls, acc = sess.run(["display_data/batch_loss:0", "display_data/accuracy:0"], feed_dict=feed_dict)
+        my_txtutils.print_validation_stats(ls, acc)
+
+
+def generate_text():
+    x = my_txtutils.convert_from_alphabet(ord("E"))
+    x = np.array([[x]])  # shape [BATCHSIZE, SEQLEN] with BATCHSIZE=1 and SEQLEN=1
+    # initial values
+    y = x
+    h = np.zeros([1, INTERNALSIZE * NLAYERS], dtype=np.float32)  # [ BATCHSIZE, INTERNALSIZE * NLAYERS]
+    for i in range(1000000000):
+        yo, h = sess.run(['softmax_layer/Yo:0', 'GRU/H:0'],
+                         feed_dict={'inputs/X:0': y, 'model/pkeep:0': 1., 'hidden_state/Hin:0': h,
+                                    'model/batchsize:0': 1})
+
+        # If sampling is be done from the topn most likely characters, the generated text
+        # is more credible. If topn is not set, it defaults to the full distribution (ALPHASIZE)
+
+        # Recommended: topn = 10 for intermediate checkpoints, topn=2 or 3 for fully trained checkpoints
+
+        c = my_txtutils.sample_from_probabilities(yo, topn=2)
+        y = np.array([[c]])  # shape [BATCHSIZE, SEQLEN] with BATCHSIZE=1 and SEQLEN=1
+        c = chr(my_txtutils.convert_to_alphabet(c))
+        print(c, end="")
+        if c == '*':
+            print("--------------------------------\n")
+
+ncnt = 0
+with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
+    new_saver = tf.train.import_meta_graph(author + '.meta')
+    new_saver.restore(sess, author)
+    validate_test()
+
